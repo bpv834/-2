53fb0f0dceae7ab15743ffaf5febed2c17b5796f
bpv834 <81566093+bpv834@users.noreply.github.com>
2021-04-03 16:13:41 +0900

    초기 커밋.

---
+++ ConsoleApplication30/ConsoleApplication30.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int width = 0;
+    int hight = 0;
+
+    printf("가로의길이");
+        scanf("%d", &width);
+
+        printf("세로의길이");
+        scanf("%d", &hight);
+
+    printf("직사각형의넓이 ");
+    printf("%d", hight * width);
+
+    printf("직사각형의둘레 ");
+    printf("%d", hight * 2 + width * 2);
+}
